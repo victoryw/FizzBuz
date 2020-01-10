73cd895cbf0a44868e7866fb6717360313f4fbc8
victoryw <tianya_wy@hotmail.com>
2020-01-10 22:47:39 +0800

    refactor: split the judement & value

--- src/test/java/fizz/buz/FizzBuz.java
+++ src/test/java/fizz/buz/FizzBuz.java
@@ -2,35 +2,45 @@ package fizz.buz;
 
 public class FizzBuz {
     public String count(int number) {
-        String result;
+
 
         final boolean isExactDivisionByThree = number % 3 == 0;
         final boolean isExactDivisionByFive = number % 5 == 0;
         final boolean isExtractDivisionBySeven = number % 7 == 0;
 
+        return generateSaying(number, isExactDivisionByThree, isExactDivisionByFive, isExtractDivisionBySeven);
+    }
+
+    private String generateSaying(int number, boolean isExactDivisionByThree, boolean isExactDivisionByFive, boolean isExtractDivisionBySeven) {
         String Fizz = "Fizz";
         String Buzz = "Buzz";
         String Whizz = "Whizz";
+        String result;
         if (isExactDivisionByFive && isExtractDivisionBySeven) {
             result = String.join("", Fizz, Whizz);
-        } else
+            return result;
+        }
         if (isExactDivisionByThree && isExtractDivisionBySeven) {
             result = String.join("", Fizz, Whizz);
-        } else if (isExactDivisionByFive && isExactDivisionByThree) {
-            result = String.join("", Fizz, Buzz);
-        } else if (isExactDivisionByThree) {
-            result = Fizz;
-        } else {
-            if (isExactDivisionByFive) {
-                result = Buzz;
-            } else {
-                if (isExtractDivisionBySeven) {
-                    result = Whizz;
-                } else {
-                    result = String.valueOf(number);
-                }
-            }
+            return result;
         }
+        if (isExactDivisionByFive && isExactDivisionByThree) {
+            result = String.join("", Fizz, Buzz);
+            return result;
+        }
+        if (isExactDivisionByThree) {
+            result = Fizz;
+            return result;
+        } else if (isExactDivisionByFive) {
+            result = Buzz;
+            return result;
+        }
+        if (isExtractDivisionBySeven) {
+            result = Whizz;
+            return result;
+        }
+
+        result = String.valueOf(number);
         return result;
     }
 }
