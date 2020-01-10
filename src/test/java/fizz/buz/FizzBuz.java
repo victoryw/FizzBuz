64064829e785471e390b6154374abf806dc57e03
victoryw <tianya_wy@hotmail.com>
2020-01-10 23:15:03 +0800

    refactor: extrac the number combine

--- src/test/java/fizz/buz/FizzBuz.java
+++ src/test/java/fizz/buz/FizzBuz.java
@@ -1,9 +1,10 @@
 package fizz.buz;
 
+import com.google.common.base.Strings;
+
 public class FizzBuz {
+
     public String count(int number) {
-
-
         final boolean isExactDivisionByThree = number % 3 == 0;
         final boolean isExactDivisionByFive = number % 5 == 0;
         final boolean isExtractDivisionBySeven = number % 7 == 0;
@@ -12,34 +13,27 @@ public class FizzBuz {
     }
 
     private String generateSaying(int number, boolean isExactDivisionByThree, boolean isExactDivisionByFive, boolean isExtractDivisionBySeven) {
+        String threeTimesSaying = threeTimesSaying(isExactDivisionByThree);
+        String fiveTimeSaying = fiveTimesSaying(isExactDivisionByFive);
+        String sevenTImeSaying = sevenTimesSaying(isExtractDivisionBySeven);
+
+        final String timesResult = String.join("", threeTimesSaying, fiveTimeSaying, sevenTImeSaying);
+
+        return Strings.isNullOrEmpty(timesResult) ? String.valueOf(number) : timesResult;
+    }
+
+    private String threeTimesSaying(boolean isExactDivisionByThree) {
         String threeSaying = "Fizz";
+        return isExactDivisionByThree ? threeSaying : "";
+    }
+
+    private String fiveTimesSaying(boolean isExtractDivisionByFive) {
         String fiveSaying = "Buzz";
+        return isExtractDivisionByFive ? fiveSaying : "";
+    }
+
+    private String sevenTimesSaying(boolean isExtractDivisionBySeven) {
         String sevenSaying = "Whizz";
-
-        if (isExactDivisionByThree && isExactDivisionByFive && isExtractDivisionBySeven) {
-            return String.join("", threeSaying, fiveSaying, sevenSaying);
-        }
-
-        if (isExactDivisionByFive && isExtractDivisionBySeven) {
-            return String.join("", fiveSaying, sevenSaying);
-        }
-        if (isExactDivisionByThree && isExtractDivisionBySeven) {
-            return String.join("", threeSaying, sevenSaying);
-        }
-        if (isExactDivisionByFive && isExactDivisionByThree) {
-            return String.join("", threeSaying, fiveSaying);
-        }
-        if (isExactDivisionByThree) {
-            return threeSaying;
-        }
-
-        if (isExactDivisionByFive) {
-            return fiveSaying;
-        }
-        if (isExtractDivisionBySeven) {
-            return sevenSaying;
-        }
-
-        return String.valueOf(number);
+        return isExtractDivisionBySeven ? sevenSaying : "";
     }
 }
